7492e5fafd8fbbbaea99594b9f08d2591d82f535
Carlos <bernardofdez2011@gmail.com>
2021-03-05 20:13:01 -0600

    Search songs by something

--- handler/music.go
+++ handler/music.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"fmt"
 	"musicDeck/database"
 	"musicDeck/structs"
 	"net/http"
@@ -44,6 +45,64 @@ func GetSongsByArtist(c *gin.Context) {
 	FROM songs s INNER JOIN  genres g ON s.genre = g.ID WHERE s.artist LIKE ?;
 	`
 	rows, err := db.Query(query, "%"+artistName+"%")
+	if err != nil {
+		c.Header("Content-Type", "application/json; charset=utf-8")
+		c.AbortWithError(400, err)
+	}
+	var song structs.Song
+	for rows.Next() {
+		// we could prettify this line with sqlx
+		err = rows.Scan(&song.ID, &song.Artist, &song.Song, &song.Length, &song.Genre.Name, &song.Genre.ID)
+		if err != nil {
+			c.Header("Content-Type", "application/json; charset=utf-8")
+			c.AbortWithError(400, err)
+		}
+		songs = append(songs, song)
+	}
+	defer db.Close()
+	c.JSON(200, songs)
+}
+
+// GetSongsBy will retrive all songs in the DB that matches the artist in the request and type
+func GetSongsBy(queryType string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		searchParam := c.Param("query")
+		db := database.InitiateConnection()
+		songs := []structs.Song{}
+		sql := `
+		SELECT s.ID, s.artist, s.song, s.length, g.name, g.ID as genreID 
+		FROM songs s INNER JOIN  genres g ON s.genre = g.ID WHERE s.` + queryType + ` LIKE '%` + searchParam + `%';`
+		fmt.Println(sql)
+		rows, err := db.Query(sql)
+		if err != nil {
+			c.Header("Content-Type", "application/json; charset=utf-8")
+			c.AbortWithError(400, err)
+		}
+		var song structs.Song
+		for rows.Next() {
+			// we could prettify this line with sqlx
+			err = rows.Scan(&song.ID, &song.Artist, &song.Song, &song.Length, &song.Genre.Name, &song.Genre.ID)
+			if err != nil {
+				c.Header("Content-Type", "application/json; charset=utf-8")
+				c.AbortWithError(400, err)
+			}
+			songs = append(songs, song)
+		}
+		defer db.Close()
+		c.JSON(200, songs)
+	}
+}
+
+// GetSongsByGenre will retrive all songs in the DB that matches the genre in the request
+func GetSongsByGenre(c *gin.Context) {
+	genreName := c.Param("query")
+	db := database.InitiateConnection()
+	songs := []structs.Song{}
+	query := `
+	SELECT s.ID, s.artist, s.song, s.length, g.name, g.ID as genreID 
+	FROM songs s INNER JOIN  genres g ON s.genre = g.ID WHERE g.name LIKE ?;
+	`
+	rows, err := db.Query(query, "%"+genreName+"%")
 	if err != nil {
 		c.Header("Content-Type", "application/json; charset=utf-8")
 		c.AbortWithError(400, err)
